e0ee45ceb113ee2846af6fee51a93bd51750c188
Cristian Santos <zcriticz.dev@gmail.com>
2024-11-29 20:58:20 -0300

    build: Add project main class

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String nameClient = "Cristian Santos";
+        String accountNumber = "123456789";
+        double balance = 2500.00;
+
+        Locale brazil = new Locale("pt", "BR");
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(brazil);
+
+        System.out.println("Bem vindo ao BankIt\n\n*******************************\nDados iniciais do cliente:\n\nNome: " + nameClient + "\n" + "Número da conta: " + accountNumber + "\n" + "Saldo: " + currencyFormatter.format(balance) + "\n" + "*******************************\n");
+
+        System.out.println("Digite a opção desejada");
+        String menuOptions = """
+                1 - Verificar saldo
+                2 - Depositar
+                3 - Transferir
+                4 - Sair
+                """;
+        System.out.println(menuOptions);
+        int options = 0;
+
+        while (options != 4) {
+            options = input.nextInt();
+            if (options == 1) {
+                System.out.println("Seu saldo atual é : " + currencyFormatter.format(balance));
+            } else if (options == 2) {
+                System.out.println("Digite o valor que deseja depositar: ");
+                double deposit = input.nextDouble();
+                balance += deposit;
+                System.out.println("Depósito realizado com sucesso! Seu saldo atual é: " + currencyFormatter.format(balance));
+            } else if (options == 3) {
+                System.out.println("Digite o valor que deseja transferir: ");
+                double transfer = input.nextDouble();
+                if (transfer > balance) {
+                    System.out.println("Saldo insuficiente para realizar a transferência");
+                } else {
+                    balance -= transfer;
+                    System.out.println("Transferência realizada com sucesso! Seu saldo atual é: " + currencyFormatter.format(balance));
+                }
+            }
+            if (options == 4) {
+                System.out.println("Obrigado por utilizar o BankIt! Volte sempre.");
+            } else {
+                System.out.println("Opção inválida");
+            }
+        }
+    }
+}
